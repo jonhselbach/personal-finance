5730cc7840d9d6e524db57555eb30dd0d4f64981
henrique <henrique.pintoluiz@gmail.com>
2025-10-06 22:24:42 -0300

    init commit

--- src/main/java/com/metodosageis/personal_finance/PersonalFinanceApplication.java
+++ src/main/java/com/metodosageis/personal_finance/PersonalFinanceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PersonalFinanceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonalFinanceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalFinanceApplication.class, args);
+    }
 
 }

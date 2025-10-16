49e37019c34f5990c62a7b68e35a5de24f25baff
henrique <henrique.pintoluiz@gmail.com>
2025-10-15 21:29:15 -0300

    Cria crud categoria;

--- src/main/java/com/metodosageis/personal_finance/service/CategoriaService.java
+++ src/main/java/com/metodosageis/personal_finance/service/CategoriaService.java
@@ -1,19 +1,26 @@
 package com.metodosageis.personal_finance.service;
 
+import com.metodosageis.personal_finance.dto.CategoriaDTO;
 import com.metodosageis.personal_finance.model.Categoria;
 import com.metodosageis.personal_finance.repository.CategoriaRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class CategoriaService {
 
-    private final CategoriaRepository repository;
+    @Autowired
+    private CategoriaRepository repository;
 
-    public CategoriaService(CategoriaRepository repository) {
-        this.repository = repository;
-    }
+    @Autowired
+    private ModelMapper modelMapper;
 
     public Categoria criarCategoria(Categoria categoria) {
         return repository.save(categoria);
@@ -21,6 +28,23 @@ public class CategoriaService {
 
     public List<Categoria> listarCategorias() {
         return repository.findAll();
+    }
+
+    public Categoria editarCategoria(CategoriaDTO dto) throws Exception {
+        Boolean temCategoria = repository.existsById(dto.getId());
+
+        if (!temCategoria) {
+            throw new Exception("Categoria não encontrada");
+        }
+
+        return repository.save(dto.toModel(modelMapper));
+    }
+
+
+    public Categoria deletarCategoria(CategoriaDTO dto) throws Exception {
+        Categoria categoria = repository.findById(dto.getId()).orElseThrow(() -> new Exception("Categoria não encontrada"));
+        repository.delete(categoria);
+        return categoria;
     }
 
     public Categoria atualizarLimite(Long id, Double novoLimite) {
